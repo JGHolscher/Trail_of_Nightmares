7b5028ec494833ff055692193c258a1789ceb2e4
Grace Holscher <111761415+JGHolscher@users.noreply.github.com>
2025-02-12 01:08:53 -0500

    Health working collision isn't

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -7,18 +7,6 @@ public class Damage : MonoBehaviour
     public playerHealth pHealth;
     public float damage;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.CompareTag("HealthDown"))
